19ad651f7078a144d9a05c1db72acf43162e184d
IT22074072 <it22074072@my.sliit.lk>
2025-01-11 13:38:50 +0530

    servlet example

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,27 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+public class App
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws LifecycleException {
         System.out.println( "Hello World!" );
+
+        // Initialize Tomcat and set the port
+        Tomcat tomcat = new Tomcat();
+
+        // Add context
+        Context context = tomcat.addContext("", null);
+
+        // Add servlet and mapping
+        Tomcat.addServlet(context, "HelloServlet", new HelloServlet());
+        context.addServletMappingDecoded("/hello", "HelloServlet");
+
+        // Start the server
+        tomcat.start();
+        tomcat.getServer().await();
+
     }
 }
